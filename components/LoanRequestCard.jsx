44f1f7bacd9b047bae04df64cc30527bfee4d770
ibrahim.a.y.m.98@gmail.com <ibrahim.a.y.m.98@gmail.com>
2025-02-14 00:20:42 +0300

    new responses for loan requests have shine

--- components/LoanRequestCard.jsx
+++ components/LoanRequestCard.jsx
@@ -85,8 +85,10 @@ export const LoanRequestCard = ({
             style={[
               styles.card,
               {
-                borderColor: isNew ? "rgb(96, 96, 96)" : "#333",
-                backgroundColor: isNew ? "rgb(53, 53, 53)" : "#2a2a2a",
+                borderColor:
+                  status === "NEW RESPONSE" ? "rgb(96, 96, 96)" : "#333",
+                backgroundColor:
+                  status === "NEW RESPONSE" ? "rgb(53, 53, 53)" : "#2a2a2a",
               },
               false && { borderLeftWidth: 4, borderLeftColor: "#FFD700" },
             ]}
